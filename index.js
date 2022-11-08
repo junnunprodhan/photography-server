921ea20fdb43fc0147ef691476fc1570d7803a20
junnun15 <junnunprodhan@gmail.com>
2022-11-08 03:28:18 -0800

    added vercel config

--- index.js
+++ index.js
@@ -10,27 +10,29 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
-// user = phusser3
-// password =KyAtkeugRfs76GkC
 
 
-
-const uri =`mongodb+srv://${process.env.DB_USER}>:${process.env.DB_PASSWORD}@cluster0.cxbomvd.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cxbomvd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+try{
+    const serviceCollection = client.db('photography').collection('services');
 
+    app.get('/services',async(req,res) =>{
+        const query ={}
+        const cursor = serviceCollection.find(query)
+        const services = await cursor.toArray()
+        res.send(services)
+    })
+    
+}
+finally{
 
+}
 
-
-
-
-
+}
+run().catch(err=>console.error(err))
 
 
 
