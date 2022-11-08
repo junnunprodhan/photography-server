ecd899ab0569115431870030e9a4d1549670eb7f
junnun15 <junnunprodhan@gmail.com>
2022-11-08 05:54:01 -0800

    dynamic api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
@@ -22,8 +22,21 @@ try{
     app.get('/services',async(req,res) =>{
         const query ={}
         const cursor = serviceCollection.find(query)
+        const services = await cursor.toArray()
+        res.send(services)
+    })
+    app.get('/service',async(req,res) =>{
+        const query ={}
+        const cursor = serviceCollection.find(query)
         const services = await cursor.limit(3).toArray()
         res.send(services)
+    })
+
+    app.get('/services/:id',async(req, res)=>{
+        const id =req.params.id;
+        const query={_id:ObjectId(id)}
+        const service=await serviceCollection.findOne(query)
+        res.send(service)
     })
     
 }
